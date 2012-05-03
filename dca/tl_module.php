66611673bca023beccdc8daa96117716f14901d6
Christoph Wiechert <wio@psitrax.de>
2012-05-03 11:40:20 +0200

    Fixed: no jumpTo field in module-settings

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -13,5 +13,5 @@
 
 
 // Palette
-$GLOBALS['TL_DCA']['tl_module']['palettes']['news4wardAuthorMenu']    = '{title_legend},name,headline,type;{config_legend},news4ward_archives;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['news4wardAuthorMenu']    = '{title_legend},name,headline,type;{config_legend},news4ward_archives;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
